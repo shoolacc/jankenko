9fe144456eec67a9edffe0f728d6b025b91156e8
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-01-31 11:21:17 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,29 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == hand._pickRandom()) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == hand._pickRandom()) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
